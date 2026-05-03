085f4f27ebcfe496628dd6aafcf8f5bca8937f1c
azametov97 <az.ametov97@gmail.com>
2026-05-03 10:52:50 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+// Файл конфигурации ссылок для сайта KAVA KAVABANGA
+// Вы можете менять ссылки в кавычках и сохранять файл. Изменения применятся автоматически.
+
+const CONFIG = {
+    telegram_url: "https://t.me/kavabangacrimea",
+    review_url: "http://review.kavabanga-dessert.ru",
+    contact_url: "https://t.me/kavabangacrimea"
+};
+
+// Экспорт для использования в index.html (не удаляйте это)
+if (typeof module !== 'undefined' && module.exports) {
+    module.exports = CONFIG;
+}
